d6e6c5e56360f8a73298cd185f06a3238e4df37a
Slava <fireweb2112@gmail.com>
2021-05-25 02:25:39 +0300

    changed icons. added score count

--- homeworks/08-functional-reactive-programming/src/index.ts
+++ homeworks/08-functional-reactive-programming/src/index.ts
@@ -1,9 +1,13 @@
-import { defer, fromEvent, interval, of, Subscription } from "rxjs";
-
-export {};
-
-import window from "./assets/window.png";
-import cat from "./assets/cat.png";
+import {
+  defer,
+  fromEvent,
+  interval,
+  of,
+  iif,
+  Subscription,
+  from,
+  combineLatest,
+} from "rxjs";
 import {
   filter,
   map,
@@ -11,63 +15,62 @@ import {
   pairwise,
   startWith,
   withLatestFrom,
+  mapTo,
+  scan,
+  mergeMap,
+  finalize,
 } from "rxjs/operators";
 import {
-  checkIfHit,
+  checkIfClicked,
+  countdown,
   drawImg,
   generateBtn,
   generateEmptyField,
   genField,
+  score,
 } from "./renderer";
+
+import window from "./assets/window.png";
+import sadCat from "./assets/sad_cat.png";
+import happyCat from "./assets/happy_cat.png";
+
+import "./index.css";
 
 const windowImg = new Image();
 windowImg.src = window;
 
-const catImg = new Image();
-catImg.src = cat;
+const sadCatImg = new Image();
+sadCatImg.src = sadCat;
 
-const getRandomExcept = (limit: number, exception: number): number => {
-  let picked: number;
-
-  do {
-    picked = Math.floor(Math.random() * limit);
-  } while (picked === exception);
-
-  return picked;
-};
+const happyCatImg = new Image();
+happyCatImg.src = happyCat;
 
 const values$ = defer(() => of(genField()));
+const interval$ = interval(1000);
 
-const renderCat$ = interval(1000).pipe(
+const renderCat$ = interval$.pipe(
   startWith(0),
   withLatestFrom(values$),
   map(([_, values]) => {
     const index = Math.floor(Math.random() * values.length);
     return values[index];
   }),
-  // pairwise(),
-  // map((value): number[] => {
-  //   const [prev, current] = value;
-  //   const [prevIndex, allValues] = prev;
-  //   const [currentIndex, allVal] = current;
-  //   if (prevIndex === currentIndex) {
-  //     const index = getRandomExcept(allVal.length, prevIndex);
-  //     return allVal[index];
-  //   }
-  //   return allVal[currentIndex];
-  // }),
   pairwise(),
   tap(([prevCoords, coords]) => {
-    drawImg(windowImg, [prevCoords[0], prevCoords[1] - 7]);
-    drawImg(catImg, [coords[0], coords[1] - 7]);
+    drawImg(windowImg, prevCoords);
+    drawImg(sadCatImg, coords);
   }),
   map((value) => value[1])
 );
 
-const click$ = fromEvent<MouseEvent>(document, "click").pipe(
-  withLatestFrom(renderCat$),
-  filter(checkIfHit),
-  tap(console.log)
+const click$ = fromEvent<MouseEvent>(document, "click");
+
+const score$ = combineLatest([click$, renderCat$]).pipe(
+  filter(checkIfClicked),
+  tap(([_, coords]) => drawImg(happyCatImg, coords)),
+  mapTo(1),
+  scan((value, count) => count + value, 0),
+  tap((value) => (score.innerHTML = value.toString()))
 );
 
 let subscription: Subscription | null = null;
@@ -75,9 +78,9 @@ let subscription: Subscription | null = null;
 fromEvent(generateBtn, "click").subscribe(() => {
   if (subscription) {
     subscription.unsubscribe();
+    score.innerHTML = "0";
   }
-
-  subscription = click$.subscribe();
+  subscription = score$.subscribe();
 });
 
 generateEmptyField();
--- homeworks/08-functional-reactive-programming/src/renderer.ts
+++ homeworks/08-functional-reactive-programming/src/renderer.ts
@@ -4,6 +4,8 @@ import wall from "./assets/wall.png";
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
 const ctx = canvas.getContext("2d")!;
 export const generateBtn = document.getElementById("generate")!;
+export const score = document.getElementById("score")!;
+export const countdown = document.getElementById("countdown")!;
 
 const startY = 50;
 const NUMBER_OF_ROWS = 10;
@@ -13,19 +15,18 @@ const random = (): number => Math.floor(Math.random() * NUMBER_OF_ROWS);
 const drawInitialImg = (image: string, coords: number[]): void => {
   const img = new Image();
   img.onload = () => {
-    ctx.drawImage(img, coords[0], coords[1]);
+    ctx.drawImage(img, coords[0], coords[1] + 10);
   };
   img.src = image;
 };
 
 export const genField = (): number[][] => {
-  console.log("generation");
   const coords: number[][] = [];
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let j = 1; j < 10; j++) {
-    for (let i = 1; i < NUMBER_OF_ROWS; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < NUMBER_OF_ROWS; i++) {
       if (i === random()) {
-        drawInitialImg(window, [50 * i, startY * j - 7]);
+        drawInitialImg(window, [50 * i, startY * j]);
         coords.push([50 * i, startY * j]);
         continue;
       }
@@ -37,23 +38,23 @@ export const genField = (): number[][] => {
 };
 
 export const generateEmptyField = (): void => {
-  for (let j = 1; j < 10; j++) {
-    for (let i = 1; i < NUMBER_OF_ROWS; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let i = 0; i < NUMBER_OF_ROWS; i++) {
       drawInitialImg(wall, [50 * i, startY * j]);
     }
   }
 };
 
 export const drawImg = (image: HTMLImageElement, coords: number[]): void =>
-  ctx.drawImage(image, coords[0], coords[1]);
+  ctx.drawImage(image, coords[0], coords[1] + 10);
 
-export const checkIfHit = ([event, coords]: [
+export const checkIfClicked = ([event, coords]: [
   event: MouseEvent,
   coords: number[]
 ]): boolean => {
-  const { clientX, clientY } = event;
+  const { offsetX, offsetY } = event;
   const [targetX, targetY] = coords;
-  const xDiff = clientX - targetX;
-  const YDiff = clientY - targetY;
+  const xDiff = offsetX - targetX;
+  const YDiff = offsetY - targetY;
   return xDiff > 0 && xDiff < 50 && YDiff > 0 && YDiff < 50;
 };
