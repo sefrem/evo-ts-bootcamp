dce488777fae091d6abe4658449bba0bce574740
Slava <fireweb2112@gmail.com>
2021-07-17 00:50:20 +0300

    testing cors

--- homeworks/blackjack/server/server.ts
+++ homeworks/blackjack/server/server.ts
@@ -17,19 +17,19 @@ const io = new Server(httpServer, {
     },
 });
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-    // Add this
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-        res.header('Access-Control-Max-Age', '120');
-        return res.status(200).json({});
-    }
-
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//
+//     // Add this
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+//         res.header('Access-Control-Max-Age', '120');
+//         return res.status(200).json({});
+//     }
+//
+//     next();
+// });
 
 io.on('connection', client => {
     client.on('newGame', (playerId: string) => {
