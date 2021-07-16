1d4e70f23db4ba6eb9c8389619f9476347cdbde2
Slava <fireweb2112@gmail.com>
2021-07-16 23:45:35 +0300

    testing cors

--- homeworks/blackjack/server/src/server.ts
+++ homeworks/blackjack/server/src/server.ts
@@ -13,9 +13,9 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: false,
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
-        allowedHeaders: ['Access-Control-Allow-Origin'],
+        credentials: true,
     },
 });
 
