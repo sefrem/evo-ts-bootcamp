0e92b60cfebc70c55d4017fe7aa19867c7c86f70
Slava <fireweb2112@gmail.com>
2021-07-18 22:56:42 +0300

    config fix

--- homeworks/blackjack/server/webpack.config.js
+++ homeworks/blackjack/server/webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: './src/types.ts',
+    entry: './src/index.ts',
     output: {
         filename: 'server.js',
         path: path.resolve(__dirname, 'dist'),
